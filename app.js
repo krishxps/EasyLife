62b2c68ed801e8848b7eff45e8f922f889a51de2
Krish Patel <99328839+krishxps@users.noreply.github.com>
2022-04-04 10:23:51 +0530

    change on bodyparser

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res){
   res.send("Hello");
